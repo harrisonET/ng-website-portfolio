500bd8253f8275f9a03020ad47f99c8958fd0dd7
Harrison E. Tanojo <harrisoneliezer@gmail.com>
2022-06-13 14:00:40 +0700

    secod commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './profile/header/header.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
+import { IntroComponent } from './profile/intro/intro.component';
+import { ProjectsComponent } from './profile/projects/projects.component';
+import { SkillsComponent } from './profile/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     HeaderComponent,
+    IntroComponent,
+    ProjectsComponent,
+    SkillsComponent,
   ],
   imports: [
     BrowserModule,
