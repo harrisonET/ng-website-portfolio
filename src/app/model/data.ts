9433028a32cec75682da62abb3cad121bc57a406
harrisonET <harrison_tanojo@mandrill.com.my>
2022-07-11 17:49:48 +0700

    add data

--- src/app/model/data.ts
+++ src/app/model/data.ts
@@ -3,13 +3,13 @@ import { Project } from './project';
 export const PROJECTS: Project[] = [
     { 
         id: 1, name: 'Krypto Android Mobile Application', 
-        desc: 'A final year project, creating an Android application for teaching students on basic Crypthography (Caesar, Viginere, etc). Developed using Java',
+        desc: 'A final year project, creating an Android application for teaching students on basic Crypthography (Caesar, Viginere, etc). Developed using Java.',
         imageUrl: 'assets/images/project-krypto.PNG', link: '', 
         tags: ['Android', 'Java'] 
     },
     { 
         id: 2, name: 'Greek Gods Card Game', 
-        desc: 'Built a turn-based card game using Java GUI',
+        desc: 'Built a turn-based card game using Java GUI that can be run in desktop.',
         imageUrl: 'assets/images/project-ggcg.PNG', link: 'https://github.com/harrisonET/Greek-Gods-Card-Game', 
         tags: ['Desktop', 'Java', 'GUI']
     }
